3301db2de5047ff62c04f713ec642149101c84bd
dabutvin <dabutvin@microsoft.com>
2016-05-30 16:15:00 -0700

    hero + fonts

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Wet eggs', cache: false });
+  res.render('index', { title: 'Wet Eggs', cache: false });
 });
 
 module.exports = router;
